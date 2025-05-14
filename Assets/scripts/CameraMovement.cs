bed797956cb8d05a5c85b1b209d6968f8142cdcd
DiegoBalseca <dbalseca@ctsformacion.com>
2025-05-15 01:28:05 +0200

    comit

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -1,48 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-{
-
-    public Transform playerTransform;
-    public Vector3 offset;
-    public Vector2 maxPosition;
-    public float interpolationRatio = 0.5f;
-    public Vector2 minPosition;
-
-
-
-    
-    void Awake()
+    public class CameraMovement : MonoBehaviour
     {
-        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
-    }
 
-    
-    void FixedUpdate()
-    {
-        if(playerTransform == null)
+        public Transform playerTransform;
+        public Vector3 offset = new Vector3(0, 0, -10);
+        public Vector2 maxPosition;
+        public float interpolationRatio = 0.5f;
+        public Vector2 minPosition;
+
+        void Start()
         {
-            return;
+            if (playerTransform != null)
+            {
+                transform.position = playerTransform.position + offset;
+            }
+        }
+                
+
+        
+
+
+        
+        void Awake()
+        {
+            playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
         }
 
-
-        Vector3 desiredPosition = playerTransform.position + offset;
-
-        float clampX = Mathf.Clamp(desiredPosition.x, minPosition.x, maxPosition.x);
-        float clampY = Mathf.Clamp(desiredPosition.y, minPosition.y, maxPosition.y);
-        Vector3 clampedPosition = new Vector3(clampX, clampY, desiredPosition.z);
-
-        Vector3 lerpedPosition = Vector3.Lerp(transform.position, clampedPosition, interpolationRatio);
-
-        transform.position = lerpedPosition;
+        
+        void FixedUpdate()
+        {
+            if(playerTransform == null)
+            {
+                return;
+            }
 
 
+            Vector3 desiredPosition = playerTransform.position + offset;
 
+            float clampX = Mathf.Clamp(desiredPosition.x, minPosition.x, maxPosition.x);
+            float clampY = Mathf.Clamp(desiredPosition.y, minPosition.y, maxPosition.y);
+            Vector3 clampedPosition = new Vector3(clampX, clampY, desiredPosition.z);
+
+            Vector3 lerpedPosition = Vector3.Lerp(transform.position, clampedPosition, interpolationRatio);
+
+            transform.position = lerpedPosition;
+
+
+
+        }
+
+        
+        
+        
     }
-
-    
-    
-    
-}
